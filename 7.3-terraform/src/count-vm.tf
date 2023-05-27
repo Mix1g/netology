3fb279f7761af2ef92a1aa5e301478f6011c8a2d
Mix1g <119140245+Mix1g@users.noreply.github.com>
2023-05-27 10:24:35 +0500

    Update count-vm.tf

--- 7.3-terraform/src/count-vm.tf
+++ 7.3-terraform/src/count-vm.tf
@@ -1,1 +1,33 @@
+data "yandex_compute_image" "ubuntu-db" {
+  family = var.vm_db_image
+}
 
+resource "yandex_compute_instance" "web" {
+ count = 2
+ name = "netology-develop-platform-web-${count.index}"
+ platform_id = "standard-v1"
+ 
+ resources {
+    cores         = var.vm_db_resources.cpu
+    memory        = var.vm_db_resources.memory
+    core_fraction = var.vm_db_resources.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-db.image_id
+      type = "network-hdd"
+      size = 5
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  metadata = local.ssh_keys_and_serial_port
+
+}
